9cdb290fb863d19aefbac4eab1de4c1a2bdf058e
Rahmattopik <151367612+Rahmattopik@users.noreply.github.com>
2025-05-27 17:21:51 +0700

    Update Civitas.js

--- Civitas.js
+++ Civitas.js
@@ -6,43 +6,43 @@ createApp({
       dekan: [
         {
           nama: "udin",
-          img: "devisi/ketua/vectorpria.jpg",
+          img: "Devisi/ketua/vectorpria.jpg",
           keterangan: "dekan fakultas teknik"
         }
       ],
       kaprodi: [
         {
           nama: "susi",
-         img: "devisi/ketua/vectorpria.jpg",
+        img: "Devisi/ketua/vectorpria.jpg",
           keterangan: "kaprodi teknologi informasi"
         }
       ],
       akademik: [
         {
           nama: "jojo",
-          img: "devisi/ketua/vectorpria.jpg",
+         img: "Devisi/ketua/vectorpria.jpg",
           keterangan: "akademik f.teknik"
         },
         {
           nama: "jiji",
-img: "devisi/ketua/vectorpria.jpg",
+ img: "Devisi/ketua/vectorpria.jpg",
           keterangan: "akademik f.teknik"
         }
       ],
       dosen: [
         {
           nama: "usop",
-          img: "devisi/ketua/vectorpria.jpg",
+        img: "Devisi/ketua/vectorpria.jpg",
           keterangan: ""
         },
         {
           nama: "lufi",
-          img: "devisi/ketua/vectorpria.jpg",
+          img: "Devisi/ketua/vectorpria.jpg",
           keterangan: ""
         },
         {
           nama: "zoro",
-          img: "devisi/ketua/vectorpria.jpg",
+         img: "Devisi/ketua/vectorpria.jpg",
           keterangan: ""
         }
       ]
