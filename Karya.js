549064eeee61ed127d427f5e95cb3531088e201c
Rahmattopik <151367612+Rahmattopik@users.noreply.github.com>
2025-06-03 19:36:34 +0700

    Update Karya.js

--- Karya.js
+++ Karya.js
@@ -9,26 +9,7 @@ createApp({
           judul: "web rahmat",
           link: "https://rahmattopik.github.io/Himatif_untara/"
         },
-                {
-          img: "img/contoh.png",
-          judul: "web rahmat",
-          link: "https://rahmattopik.github.io/Himatif_untara/"
-        },
-                {
-          img: "img/contoh.png",
-          judul: "web rahmat",
-          link: "https://rahmattopik.github.io/Himatif_untara/"
-        },
-              {
-          img: "img/contoh.png",
-          judul: "web rahmat",
-          link: "https://rahmattopik.github.io/Himatif_untara/"
-        },
-                {
-          img: "img/contoh.png",
-          judul: "web rahmat",
-          link: "https://rahmattopik.github.io/Himatif_untara/"
-        },
+                
         // Tambahkan item lainnya jika perlu
       ]
     };
